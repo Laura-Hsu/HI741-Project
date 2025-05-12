9d57205041a9ebc5097419bed2ed941d5fcbce7d
TZULUN HSU <55292407+Laura-Hsu@users.noreply.github.com>
2025-05-12 07:24:00 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from gui import LoginWindow
 
 if __name__ == "__main__":
-    print("Launching login window...")  # 測試輸出
     root = tk.Tk()
     LoginWindow(root)
     root.mainloop()
